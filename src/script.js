98dfedc3c1c32bff83d8339532a2687282510608
Vanda <adnav3@users.noreply.github.com>
2020-11-24 22:15:38 +0000

    Update script.js
    - clear form after submition
    - isolated API call, in order to set a default city when page loads
    - lisbon chosen as default city for page load

--- src/script.js
+++ src/script.js
@@ -2,18 +2,25 @@ function currentWeather(response) {
   console.log(response);
 }
 
-function cityName(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-search").value;
+function cityName(city) {
   if (city) {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(url).then(currentWeather);
+    document.querySelector("#search-form").reset();
   } else {
     alert("Please enter a city name 🌞");
   }
 }
 
+function searchResult(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-search").value;
+  cityName(city);
+}
+
 let citySearch = document.querySelector("#search-form");
-citySearch.addEventListener("submit", cityName);
+citySearch.addEventListener("submit", searchResult);
 
 let apiKey = "adc7ca37eff3fa0b73ae6b54ab48c90d";
+
+cityName("lisbon");
