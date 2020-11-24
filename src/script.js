ce33e6c3327549a3bef4f514b45ec065f85441b0
Vanda <adnav3@users.noreply.github.com>
2020-11-24 20:37:31 +0000

    Search functionality and API call
    Built search functionality, to make an API call based on form input value

--- src/script.js
+++ src/script.js
@@ -1,1 +1,19 @@
+function currentWeather(response) {
+  console.log(response);
+}
 
+function cityName(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-search").value;
+  if (city) {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(currentWeather);
+  } else {
+    alert("Please enter a city name 🌞");
+  }
+}
+
+let citySearch = document.querySelector("#search-form");
+citySearch.addEventListener("submit", cityName);
+
+let apiKey = "adc7ca37eff3fa0b73ae6b54ab48c90d";
