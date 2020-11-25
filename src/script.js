1f00df80ec24acfa0841c327406ff0a77d2f7e60
Vanda <adnav3@users.noreply.github.com>
2020-11-25 00:32:14 +0000

    Current weather processing added
    - created function to process API data and imbue it into page
    - weather icon placeholder introduced
    - weather icon styling
    - removed fake placeholder data

--- src/script.js
+++ src/script.js
@@ -1,5 +1,53 @@
-function currentWeather(response) {
+function getWeekday(date) {}
+
+function getTime(time, timezone) {}
+
+function forecast(response) {
   console.log(response);
+}
+
+function currentWeather(response) {
+  let location = document.querySelector("#search-result");
+  location.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+
+  let dateTime = response.data.dt;
+  let timezone = response.data.timezone;
+  getWeekday(dateTime);
+  getTime(dateTime, timezone);
+
+  let temperature = document.querySelector("#temp-now");
+  temperature.innerHTML = `${Math.round(response.data.main.temp)}`;
+
+  let weatherIcon = document.querySelector("#wthr-now-symbol");
+  weatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  weatherIcon.setAttribute("alt", `${response.data.weather[0].description}`);
+
+  let weatherDescription = document.querySelector("#current-weather");
+  weatherDescription.innerHTML = `${response.data.weather[0].description}`;
+
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `${response.data.main.humidity}`;
+
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `${response.data.wind.speed}`;
+
+  let precipitation = document.querySelector("#precipitation");
+  if (response.data.rain === undefined) {
+    precipitation.innerHTML = 0;
+  } else {
+    precipitation.innerHTML = `${response.data.rain["1h"]}`;
+  }
+
+  let lat = response.data.coord.lat;
+  let long = response.data.coord.lon;
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=metric`
+    )
+    .then(forecast);
 }
 
 function cityName(city) {
